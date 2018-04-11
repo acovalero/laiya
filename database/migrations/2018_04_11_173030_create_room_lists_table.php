a76c695aa6f349bf6f73c84c8e045f0a3c2569d9
acovalero <acovalero18@gmail.com>
2018-04-12 01:34:29 +0800

    Migration Skeleton

---
+++ database/migrations/2018_04_11_173030_create_room_lists_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRoomListsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('room_lists', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('room_lists');
+    }
+}
