2bf0c5ad0e41d5387fe31e029124d95f4a840df9
renzglorioso <renzglorioso@github.com>
2018-04-13 10:30:47 +0800

    asdfjk

--- resources/views/admin/inquiries/edit.blade.php
+++ resources/views/admin/inquiries/edit.blade.php
@@ -6,74 +6,124 @@
     {!! Form::model($inquiry, ['method' => 'PUT', 'route' => ['admin.inquiries.update', $inquiry->id]]) !!}
 
     <div class="panel panel-default">
-        <div class="panel-heading">
-            @lang('quickadmin.qa_edit')
+            <div class="panel-heading">
+                @lang('quickadmin.qa_create')
+            </div> 
+            <div class="panel-body">
+                {{-- Customer --}}
+                <div class="row">
+                    <div class="col-xs-12 form-group">
+                        {!! Form::label('customer_id', trans('quickadmin.inquiries.fields.customer').'', ['class' => 'control-label']) !!}
+                        {!! Form::select('customer_id', $customers, old('customer_id'), ['class' => 'form-control select2']) !!}
+                        <p class="help-block"></p>
+                        @if($errors->has('customer_id'))
+                            <p class="help-block">
+                                {{ $errors->first('customer_id') }}
+                            </p>
+                        @endif
+                    </div>
+                </div>
+    
+                <!-- Room Selection -->
+                {{-- <div class="row">
+                    <div class="col-xs-12 form-group">
+                        {!! Form::label('rooms_id', trans('quickadmin.inquiries.fields.room').'', ['class' => 'control-label']) !!}
+                        {!! Form::select('rooms_id', $rooms, old('rooms_id'), ['class' => 'form-control select2']) !!}
+                        <p class="help-block"></p>
+                        @if($errors->has('rooms_id'))
+                            <p class="help-block">
+                                {{ $errors->first('rooms_id') }}
+                            </p>
+                        @endif
+                    </div>
+                </div> --}}
+    
+                <!-- Check in -->
+                <div class="row">
+                    <div class="col-xs-12 form-group">
+                        {!! Form::label('time_from', trans('quickadmin.inquiries.fields.time-from').'*', ['class' => 'control-label']) !!}
+                        {!! Form::text('time_from', old('time_from'), ['id' => 'checkin', 'class' => 'form-control datetimepicker', 'placeholder' => '', 'required' => '']) !!}
+                        <p class="help-block"></p>
+                        @if($errors->has('time_from'))
+                            <p class="help-block">
+                                {{ $errors->first('time_from') }}
+                            </p>
+                        @endif
+                    </div>
+                </div>
+                {{-- Check out --}}
+                <div class="row">
+                    <div class="col-xs-12 form-group">
+                        {!! Form::label('time_to', trans('quickadmin.inquiries.fields.time-to').'*', ['class' => 'control-label']) !!}
+                        {!! Form::text('time_to', old('time_to'), ['id' => 'checkout', 'class' => 'form-control datetimepicker', 'placeholder' => '', 'required' => '']) !!}
+                        <p class="help-block"></p>
+                        @if($errors->has('time_to'))
+                            <p class="help-block">
+                                {{ $errors->first('time_to') }}
+                            </p>
+                        @endif
+                    </div>
+                </div>
+            </div>  
         </div>
-
-        <div class="panel-body">
-            <div class="row">
-                <div class="col-xs-12 form-group">
-                    {!! Form::label('customer_id', trans('quickadmin.inquiries.fields.customer').'', ['class' => 'control-label']) !!}
-                    {!! Form::select('customer_id', $customers, old('customer_id'), ['class' => 'form-control select2']) !!}
-                    <p class="help-block"></p>
-                    @if($errors->has('customer_id'))
-                        <p class="help-block">
-                            {{ $errors->first('customer_id') }}
-                        </p>
-                    @endif
+      
+        {{-- QUOTATION --}}
+        
+        <h3 class="page-title">@lang('quickadmin.quotations.title')</h3>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                @lang('quickadmin.qa_create')
+            </div> 
+            <div class="panel-body">
+    
+                <!-- Room Selection -->
+                <div class="row">
+                    <div class="col-xs-12 form-group">
+                        {!! Form::label('rooms_id', trans('quickadmin.quotations.fields.room').'', ['class' => 'control-label']) !!}
+                        {!! Form::select('rooms_id', $rooms, old('rooms_id'), ['class' => 'form-control select2']) !!}
+                        <p class="help-block"></p>
+                        @if($errors->has('rooms_id'))
+                            <p class="help-block">
+                                {{ $errors->first('rooms_id') }}
+                            </p>
+                        @endif
+                    </div>
                 </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12 form-group">
-                    {!! Form::label('rooms_id', trans('quickadmin.inquiries.fields.room').'', ['class' => 'control-label']) !!}
-                    {!! Form::select('rooms_id', $rooms, old('rooms_id'), ['class' => 'form-control select2']) !!}
-                    <p class="help-block"></p>
-                    @if($errors->has('rooms_id'))
-                        <p class="help-block">
-                            {{ $errors->first('rooms_id') }}
-                        </p>
-                    @endif
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12 form-group">
-                    {!! Form::label('time_from', trans('quickadmin.inquiries.fields.time-from').'*', ['class' => 'control-label']) !!}
-                    {!! Form::text('time_from', old('time_from'), ['class' => 'form-control datetimepicker', 'placeholder' => '', 'required' => '']) !!}
-                    <p class="help-block"></p>
-                    @if($errors->has('time_from'))
-                        <p class="help-block">
-                            {{ $errors->first('time_from') }}
-                        </p>
-                    @endif
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12 form-group">
-                    {!! Form::label('time_to', trans('quickadmin.inquiries.fields.time-to').'*', ['class' => 'control-label']) !!}
-                    {!! Form::text('time_to', old('time_to'), ['class' => 'form-control datetimepicker', 'placeholder' => '', 'required' => '']) !!}
-                    <p class="help-block"></p>
-                    @if($errors->has('time_to'))
-                        <p class="help-block">
-                            {{ $errors->first('time_to') }}
-                        </p>
-                    @endif
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12 form-group">
-                    {!! Form::label('additional_information', trans('quickadmin.inquiries.fields.additional-information').'*', ['class' => 'control-label']) !!}
-                    {!! Form::textarea('additional_information', old('additional_information'), ['class' => 'form-control ', 'placeholder' => '', 'required' => '']) !!}
-                    <p class="help-block"></p>
-                    @if($errors->has('additional_information'))
-                        <p class="help-block">
-                            {{ $errors->first('additional_information') }}
-                        </p>
-                    @endif
-                </div>
-            </div>
             
+                <!-- Pax  -->
+                <div class="row">
+                    <div class="col-xs-12 form-group">
+                        {!! Form::label('pax', trans('quickadmin.quotations.fields.pax').'*', ['class' => 'control-label']) !!}
+                        {!! Form::number('pax', old('pax'), ['class' => 'form-control', 'placeholder' => '', 'required' => '']) !!}
+                        <p class="help-block"></p>
+                        @if($errors->has('pax'))
+                            <p class="help-block">
+                                {{ $errors->first('pax') }}
+                            </p>
+                        @endif
+                    </div>
+                </div>
+    
+                <!-- Fee Selection -->
+                <div class="row">
+                    <div class="col-xs-12 form-group">
+                        {!! Form::label('fees_id', trans('quickadmin.quotations.fields.fee').'', ['class' => 'control-label']) !!}
+                        {!! Form::select('fees_id', $fees, old('fees_id'), ['class' => 'form-control select2']) !!}
+                        <p class="help-block"></p>
+                        @if($errors->has('fees_id'))
+                            <p class="help-block">
+                                {{ $errors->first('fees_id') }}
+                            </p>
+                        @endif
+                    </div>
+                </div>
+    
+                
+    
+        
+            </div>
         </div>
-    </div>
+    
 
     {!! Form::submit(trans('quickadmin.qa_update'), ['class' => 'btn btn-danger']) !!}
     {!! Form::close() !!}
