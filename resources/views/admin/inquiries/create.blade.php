319e55e1de9cf24075c10c2fdde2436a6c7092af
renzglorioso <renzglorioso@github.com>
2018-04-13 15:16:58 +0800

    js multiple lines in inquiries/create

--- resources/views/admin/inquiries/create.blade.php
+++ resources/views/admin/inquiries/create.blade.php
@@ -124,8 +124,99 @@
             </div>
 
 
+            <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Student Name</th>
+                            <th>Father Name</th>
+                            <th>Roll No</th>
+                            <th>Obtained Marks</th>
+                            <th>Total Marks</th>
+                            <th>%</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody class="resultbody">
+                        <tr>
+                            <td class="no">1</td>
+                            <td>
+                                <input type="text" class="name form-control" name="name[]" value="{{ old('name') }}">
+                            </td>
+                            <td>
+                                <input type="text" class="fname form-control" name="fname[]" value="{{ old('fname') }}">
+                            </td>
+                            <td>
+                                <input type="text" class="rollno form-control" name="rollno[]" value="{{ old('rollno') }}">
+                            </td>
+                            <td>
+                                <input type="text" class="obtainedmarks form-control" name="obtainedmarks[]" value="{{ old('email') }}">
+                            </td>
+                            <td>
+                                <input type="text" class="totalmarks form-control" name="totalmarks[]">
+                            </td>
+                            <td>
+                                <input type="text" class="percentage form-control" name="percentage[]">
+                            </td>
+                            <td>
+                                <input type="button" class="btn btn-danger delete" value="x">
+                            </td>
+                        </tr>
 
-            <!-- Room Selection -->
+                    </tbody>
+                </table>
+                <center><input type="button" class="btn btn-lg btn-primary add" value="Add New Item">
+
+
+
+                <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Student Name</th>
+                                <th>Father Name</th>
+                                <th>Roll No</th>
+                                <th>Obtained Marks</th>
+                                <th>Total Marks</th>
+                                <th>%</th>
+                                <th>Delete</th>
+                            </tr>
+                        </thead>
+                        <tbody class="resultbody">
+                            <tr>
+                                <td class="no">1</td>
+                                <td>
+                                    <input type="text" class="name form-control" name="name[]" value="{{ old('name') }}">
+                                </td>
+                                <td>
+                                    <input type="text" class="fname form-control" name="fname[]" value="{{ old('fname') }}">
+                                </td>
+                                <td>
+                                    <input type="text" class="rollno form-control" name="rollno[]" value="{{ old('rollno') }}">
+                                </td>
+                                <td>
+                                    <input type="text" class="obtainedmarks form-control" name="obtainedmarks[]" value="{{ old('email') }}">
+                                </td>
+                                <td>
+                                    <input type="text" class="totalmarks form-control" name="totalmarks[]">
+                                </td>
+                                <td>
+                                    <input type="text" class="percentage form-control" name="percentage[]">
+                                </td>
+                                <td>
+                                    <input type="button" class="btn btn-danger delete" value="x">
+                                </td>
+                            </tr>
+    
+                        </tbody>
+                    </table>
+                    <center><input type="button" class="btn btn-lg btn-primary add" value="Add New Item">
+
+
+
+
+
+            {{-- <!-- Room Selection -->
             <div class="row">
                 <div class="col-xs-12 form-group">
                     {!! Form::label('rooms_id', trans('quickadmin.quotations.fields.room').'', ['class' => 'control-label']) !!}
@@ -208,7 +299,7 @@
                         </p>
                     @endif
                 </div>
-            </div>
+            </div> --}}
 
 
 
@@ -256,6 +347,22 @@
                 alert("Im sorry , please choose the present or future date for reservation");
                 return false;
             }
+        });
+
+        $('.add').click(function () {
+            var n = ($('.resultbody tr').length - 0) + 1;
+            var tr = '<tr><td class="no">' + n + '</td>' +
+                    '<td><input type="text" class="name form-control" name="name[]" value="{{ old('name') }}"></td>'+
+                    '<td><input type="text" class="fname form-control" name="fname[]" value="{{ old('fname') }}"></td>'+
+                    '<td><input type="text" class="rollno form-control" name="rollno[]" value="{{ old('rollno') }}"></td>'+
+                    '<td><input type="text" class="obtainedmarks form-control" name="obtainedmarks[]" value="{{ old('email') }}"></td>'+
+                    '<td><input type="text" class="totalmarks form-control" name="totalmarks[]"></td>'+
+                    '<td><input type="text" class="percentage form-control" name="percentage[]"></td>'+
+                    '<td><input type="button" class="btn btn-danger delete" value="x"></td></tr>';
+            $('.resultbody').append(tr);
+        });
+        $('.resultbody').delegate('.delete', 'click', function () {
+            $(this).parent().parent().remove();
         });
 
         });
